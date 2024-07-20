305e7c4677ed94936d572ab3e31fa22e47b088ff
favoured mwange <mwangefavoured@gmail.com>
2024-07-20 20:30:12 +0300

    Adds fetch

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(res=>res.json())
-  .then(Data=>{renderBooks(Data)})
+
+return fetch("https://anapioficeandfire.com/api/books")
+.then(function(response){
+   response.json()
+   .then(function(data){
+     renderBooks(data)})
+ })
 }
 
 function renderBooks(books) {
